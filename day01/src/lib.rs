1c3c9b252db199e78ac6403a3d4115a512c5910a
PurpleMyst <PurpleMyst@users.noreply.github.com>
2021-12-02 00:06:32 +0100

    perf(day01): implement algebraic semplification

--- day01/src/lib.rs
+++ day01/src/lib.rs
@@ -8,10 +8,9 @@ pub fn solve() -> (impl Display, impl Display) {
         .collect();
 
     let p1 = report.windows(2).filter(|w| w[0] < w[1]).count();
-    let p2 = report
-        .windows(4)
-        .filter(|w| w[..3].into_iter().sum::<u16>() < w[1..][..3].into_iter().sum())
-        .count();
+
+    // a + b + c < b + c + d <=> a < d
+    let p2 = report.windows(4).filter(|w| w[0] < w[3]).count();
 
     (p1, p2)
 }
