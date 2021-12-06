4cf5da71fd8829d9caa71b03abf8d0c38898041f
PurpleMyst <PurpleMyst@users.noreply.github.com>
2021-12-06 14:36:18 +0100

    ci: run iai

---
+++ benchmark/benches/iai.rs
@@ -0,0 +1,15 @@
+use day01::solve as day01_solve;
+use day02::solve as day02_solve;
+use day03::solve as day03_solve;
+use day04::solve as day04_solve;
+use day05::solve as day05_solve;
+use day06::solve as day06_solve;
+
+iai::main!(
+    day01_solve,
+    day02_solve,
+    day03_solve,
+    day04_solve,
+    day05_solve,
+    day06_solve,
+);
