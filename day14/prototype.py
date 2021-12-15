0817d49e4537e1cb2125bce128f607e2d0010c6c
PurpleMyst <PurpleMyst@users.noreply.github.com>
2021-12-15 20:30:46 +0100

    feat: solve day 14 in python

---
+++ day14/prototype.py
@@ -0,0 +1,54 @@
+from pathlib import Path
+import typing as t
+
+import numpy as np
+import numpy.typing as npt
+
+
+def main() -> None:
+    # Parse the input
+    inp = Path("src/input.txt").read_text()
+    [template, rules] = inp.split("\n\n")
+    rules = dict(rule.split(" -> ") for rule in rules.splitlines())
+
+    # Make an order for all possible pairs
+    order = sorted(rules.keys())
+
+    # Construct a matrix that maps each rule AB -> C to the pairs AC and CB that it produces.
+    # Each column corresponds to one pair.
+    step_matrix = np.zeros((len(order), len(order)), dtype=np.int64)
+    for i, pair in enumerate(order):
+        new = rules[pair]
+        products = pair[0] + new, new + pair[1]
+        for product in products:
+            assert product in order
+            step_matrix[order.index(product), i] += 1
+
+    # Construct the vector corresponding to the pairs in our template
+    template_matrix = np.zeros(len(order), dtype=np.int64)
+    for pair in zip(template, template[1:]):
+        template_matrix[order.index("".join(pair))] += 1
+
+    def solve(steps: int) -> int:
+        # Exponentiate our matrix to the number of steps required.
+        steps_matrix = np.linalg.matrix_power(step_matrix, steps)
+
+        # Apply our matrix to the template.
+        state = steps_matrix @ template_matrix
+
+        # Calculate the frequency of each element.
+        # We only count the first item of each pair to avoid double-counting: this means
+        # we'd skip over the last element in the polymer, so we handle that edge case
+        # specially.
+        freq = {template[-1]: 1}
+        for pair, count in zip(order, state):
+            freq[pair[0]] = freq.get(pair[0], 0) + count
+
+        # Get the answer
+        return max(freq.values()) - min(freq.values())
+
+    print(solve(10))
+    print(solve(40))
+
+if __name__ == "__main__":
+    main()
--- day14/src/lib.rs
+++ day14/src/lib.rs
@@ -1,39 +1,87 @@
-use std::{collections::HashMap, fmt::Display};
+use std::{collections::Vec, fmt::Display};
 
-#[inline]
-pub fn solve() -> (impl Display, impl Display) {
-    let (template, rules) = include_str!("input.txt")
-        .trim()
-        .split_once("\n\n")
-        .unwrap();
+struct Rules([Option<u8>; 26 * 26]);
 
-    let rules: HashMap<_, _> = rules
-        .trim()
-        .lines()
-        .map(|line| {
-            let (k, v) = line.split_once(" -> ").unwrap();
-            (k.as_bytes(), v.as_bytes()[0])
-        })
-        .collect();
+impl Rules {
+    fn new(rules: &str) -> Self {
+        let mut inserts = [None; 26 * 26];
+        rules
+            .lines()
+            .map(|line| line.split_once(" -> ").unwrap())
+            .for_each(|(lhs, rhs)| {
+                inserts[usize::from(lhs.as_bytes()[0] - b'A') * 26
+                    + usize::from(lhs.as_bytes()[1] - b'A')] = Some(rhs.as_bytes()[0] - b'A');
+            });
+        Self(inserts)
+    }
 
-    let mut template = template.as_bytes().to_vec();
+    fn get(&self, (fst, snd): (u8, u8)) -> Option<u8> {
+        self.0[usize::from(fst) * 26 + usize::from(snd)]
+    }
+}
 
-    for _ in 0..40 {
-        let mut i = 0;
-        while i < template.len() - 1 {
-            if let Some(new) = rules.get(&template[i..i + 2]) {
-                template.insert(i + 1, *new);
-                i += 1;
-            }
+fn step(rules: &Rules, polymer: &mut Vec<u8>) {
+    let mut i = 0;
+    while i < polymer.len() - 1 {
+        let fst = polymer[i];
+        let snd = polymer[i + 1];
+        if let Some(new) = rules.get((fst, snd)) {
+            polymer.insert(i + 1, new);
             i += 1;
+        }
+        i += 1;
+    }
+}
+
+struct Computer {
+    lut: [[usize; 26]; 26 * 26],
+    rules: Rules,
+}
+
+fn tally(frequency: &mut [usize; 26], polymer: &[u8]) {
+    for &ch in polymer {
+        frequency[usize::from(ch)] += 1;
+    }
+}
+
+impl Computer {
+    fn new(rules: Rules) -> Self {
+        Self {
+            lut: [[0; 26]; 26 * 26],
+            rules,
         }
     }
 
-    let mut freq = [None; 26];
-    for ch in template {
-        *freq[usize::from(ch - b'A')].get_or_insert(0) += 1
+    fn run_toplevel(&mut self, template: &[u8], generations: usize) {
+        template
+            .windows(2)
+            .for_each(|pair| self.run_pair(pair.try_into().unwrap(), generations));
     }
-    let p1 = freq.iter().filter_map(|&k| k).max().unwrap() - freq.iter().filter_map(|&k| k).min().unwrap();
 
-    (p1, "TODO")
+    fn run_pair(&mut self, [fst, snd]: [u8; 2], generations: usize) {
+        let mut polymer = vec![fst, snd];
+
+        for _ in 0..generations {
+            step(&self.rules, &mut polymer);
+        }
+
+        tally(&mut self.lut[fst as usize * 26 + snd as usize], &polymer);
+    }
+
+}
+
+#[inline]
+pub fn solve() -> (impl Display, impl Display) {
+    let (template, rules) = include_str!("input.txt").trim().split_once("\n\n").unwrap();
+
+    let rules = Rules::new(rules);
+
+    let mut template = template.as_bytes().to_vec();
+    template.iter_mut().for_each(|ch| *ch -= b'A');
+
+    let mut computer = Computer::new(rules);
+
+    computer.run_toplevel(&template, 10);
+
+    ("TODO", "TODO")
 }
