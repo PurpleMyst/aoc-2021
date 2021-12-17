d8180e37cc2197c746f78f36ab3b4e59b37c1a84
PurpleMyst <PurpleMyst@users.noreply.github.com>
2021-12-17 16:14:39 +0100

    perf(day17): utilize algebraic optimization for part 1

--- day17/src/lib.rs
+++ day17/src/lib.rs
@@ -18,17 +18,14 @@ fn simulate(mut vx: i64, mut vy: i64) -> impl Iterator<Item = (i64, i64)> {
     })
 }
 
-fn predict(vx: i64, vy: i64, tx1: i64, tx2: i64, ty1: i64, ty2: i64) -> Option<i64> {
-    let mut ymax = 0;
+fn lands(vx: i64, vy: i64, tx1: i64, tx2: i64, ty1: i64, ty2: i64) -> bool {
     simulate(vx, vy)
-        .inspect(|&(_, y)| ymax = ymax.max(y))
         .find_map(|(x, y)| {
             ((tx1..=tx2).contains(&x) && (ty1..=ty2).contains(&y))
                 .then(|| true)
                 .or_else(|| (x > tx2 || (y < 0 && y.abs() > ty1.abs())).then(|| false))
         })
         .unwrap()
-        .then(|| ymax)
 }
 
 #[inline]
@@ -45,12 +42,11 @@ pub fn solve() -> (impl Display, impl Display) {
     let (ty1, ty2) = parse_range(ty);
     debug_assert!(ty2 < 0);
 
-    let mut p1 = 0;
+    let p1 = ty1.abs() * (ty1.abs() - 1) / 2;
     let mut p2 = 0;
     for vx in 0..=tx2 {
         for vy in ty2 * 2..=ty2 * -2 {
-            if let Some(ymax) = predict(vx, vy, tx1, tx2, ty1, ty2) {
-                p1 = p1.max(ymax);
+            if lands(vx, vy, tx1, tx2, ty1, ty2) {
                 p2 += 1;
             }
         }
