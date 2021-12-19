29946c8f5663f9095a192690933030e3b9d1bf54
PurpleMyst <PurpleMyst@users.noreply.github.com>
2021-12-19 03:53:00 +0100

    perf(day18): better representation
    instead of whatever that mess was, just a linear list.
    
    next step is to use a linked list or something similiar.

--- day18/src/lib.rs
+++ day18/src/lib.rs
@@ -1,151 +1,169 @@
 use std::fmt::Display;
-use std::ops::ControlFlow;
 
-use slab::Slab;
-
-// TODO: utilize a tree so that we can mutate it directly
-
-#[derive(Debug, Clone, PartialEq)]
-enum Structure {
-    Single(usize),
-    Pair(Box<Structure>, Box<Structure>),
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+enum Item {
+    LeftBracket,
+    RightBracket,
+    Number(u64),
 }
 
-fn load_input<'a>(input: &'a [u8], slab: &mut Slab<u64>) -> (&'a [u8], Structure) {
-    let (head, input) = input.split_first().unwrap();
-    match head {
-        b'[' => {
-            let (input, fst) = load_input(input, slab);
-            let (&ch, input) = input.split_first().unwrap();
-            assert_eq!(ch, b',');
-            let (input, snd) = load_input(input, slab);
-            let (&ch, input) = input.split_first().unwrap();
-            assert_eq!(ch, b']');
-            (input, Structure::Pair(Box::new(fst), Box::new(snd)))
+impl Display for Item {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Item::LeftBracket => write!(f, "[ "),
+            Item::RightBracket => write!(f, "] "),
+            Item::Number(n) => write!(f, "{} ", n),
         }
-        b']' => unreachable!(),
-        b',' => unreachable!(),
-        &n => (input, Structure::Single(slab.insert(u64::from(n - b'0')))),
     }
 }
 
-#[derive(Debug, Default)]
-struct State {
-    left: Option<usize>,
-    carry: Option<usize>,
-    level: usize,
-}
-
-fn mag(slab: &Slab<u64>, cake: &Structure) -> u64 {
-    match cake {
-        Structure::Single(n) => slab[*n],
-        Structure::Pair(fst, snd) => 3 * mag(slab, fst) + 2 * mag(slab, snd),
+impl Item {
+    fn try_into_number(&mut self) -> Option<&mut u64> {
+        if let Self::Number(v) = self {
+            Some(v)
+        } else {
+            None
+        }
     }
 }
 
-fn explode_step(
-    slab: &mut Slab<u64>,
-    cake: &mut Structure,
-    state: State,
-) -> ControlFlow<(), State> {
-    match cake {
-        Structure::Single(n) => {
-            if let Some(m) = state.carry {
-                slab[*n] += slab[m];
-                ControlFlow::Break(())
-            } else {
-                ControlFlow::Continue(State {
-                    left: Some(*n),
-                    ..state
-                })
+fn load_input(input: &[u8]) -> Vec<Item> {
+    input
+        .iter()
+        .filter_map(|b| {
+            Some(match b {
+                b'[' => Item::LeftBracket,
+                b']' => Item::RightBracket,
+                b',' => return None,
+                _ => Item::Number((b - b'0').into()),
+            })
+        })
+        .collect()
+}
+
+fn mag(n: &[Item]) -> u64 {
+    let mut stack: Vec<(Option<u64>, Option<u64>)> = Vec::new();
+
+    for item in n {
+        match item {
+            Item::LeftBracket => stack.push((None, None)),
+            Item::RightBracket => {
+                let (a, b) = stack.pop().unwrap();
+                let m = 3 * a.unwrap() + 2 * b.unwrap();
+                match stack.last_mut() {
+                    Some((a @ None, _)) => *a = Some(m),
+                    Some((_, b @ None)) => *b = Some(m),
+                    Some((_, _)) => unreachable!(),
+                    None => return m,
+                }
             }
+            Item::Number(m) => match stack.last_mut().unwrap() {
+                (a @ None, _) => *a = Some(*m),
+                (_, b @ None) => *b = Some(*m),
+                (_, _) => unreachable!(),
+            },
         }
+    }
 
-        Structure::Pair(fst, snd) => {
-            if state.carry.is_none() && state.level == 4 {
-                if let Some(left) = state.left {
-                    slab[left] += match &**fst {
-                        Structure::Single(n) => slab[*n],
-                        _ => unreachable!(),
+    unreachable!();
+}
+
+fn explode(n: &mut Vec<Item>) -> bool {
+    let mut left = None;
+    let mut depth = 0;
+
+    for (idx, item) in n.iter().enumerate() {
+        match item {
+            Item::LeftBracket => {
+                if depth != 4 {
+                    depth += 1;
+                    continue;
+                }
+
+                let mut it = n.drain(idx..idx + 4);
+                assert_eq!(it.next(), Some(Item::LeftBracket));
+                let a = *it.next().unwrap().try_into_number().unwrap();
+                let b = *it.next().unwrap().try_into_number().unwrap();
+                assert_eq!(it.next(), Some(Item::RightBracket));
+                drop(it);
+
+                if let Some(left) = left {
+                    if let Some(left) = Item::try_into_number(&mut n[left]) {
+                        *left += a;
                     }
                 }
-                let right = match &**snd {
-                    Structure::Single(n) => *n,
-                    _ => unreachable!(),
-                };
-                *cake = Structure::Single(slab.insert(0));
-                ControlFlow::Continue(State {
-                    carry: Some(right),
-                    ..state
-                })
-            } else {
-                let state = State {
-                    level: state.level + 1,
-                    ..state
-                };
-                let state = explode_step(slab, fst, state)?;
-                let mut state = explode_step(slab, snd, state)?;
-                state.level -= 1;
-                ControlFlow::Continue(state)
+
+                if let Some(right) = n.iter_mut().skip(idx).find_map(Item::try_into_number) {
+                    *right += b;
+                }
+
+                n.insert(idx, Item::Number(0));
+
+                return true;
             }
+            Item::RightBracket => depth -= 1,
+            Item::Number(..) => left = Some(idx),
         }
     }
+
+    false
 }
 
-fn split_step(slab: &mut Slab<u64>, cake: &mut Structure) -> ControlFlow<()> {
-    match cake {
-        Structure::Single(n) => {
-            let n = *n;
-            if slab[n] >= 10 {
-                *cake = Structure::Pair(
-                    Box::new(Structure::Single(slab.insert(slab[n] / 2))),
-                    Box::new(Structure::Single(slab.insert((slab[n] + 1) / 2))),
-                );
-                ControlFlow::Break(())
-            } else {
-                ControlFlow::Continue(())
-            }
-        }
+fn split(n: &mut Vec<Item>) -> bool {
+    for (idx, item) in n.iter().enumerate() {
+        match item {
+            Item::Number(m) => {
+                let m = *m;
+                if m < 10 {
+                    continue;
+                }
+                n.remove(idx);
 
-        Structure::Pair(fst, snd) => {
-            split_step(slab, fst)?;
-            split_step(slab, snd)
+                let a = m / 2;
+                let b = (m + 1) / 2;
+                [
+                    Item::LeftBracket,
+                    Item::Number(a),
+                    Item::Number(b),
+                    Item::RightBracket,
+                ]
+                .into_iter()
+                .rev()
+                .for_each(|x| n.insert(idx, x));
+
+                return true;
+            }
+
+            Item::LeftBracket | Item::RightBracket => {}
         }
     }
+    false
 }
 
-fn reduce(slab: &mut Slab<u64>, cake: &mut Structure) {
-    loop {
-        if matches!(
-            explode_step(slab, cake, State::default()),
-            ControlFlow::Continue(..)
-        ) && split_step(slab, cake) == ControlFlow::Continue(())
-        {
-            break;
-        }
-    }
+fn reduce(n: &mut Vec<Item>) {
+    while explode(n) || split(n) {}
 }
 
 #[inline]
 pub fn solve() -> (impl Display, impl Display) {
-    let mut slab = Slab::new();
     let terms = include_str!("input.txt")
         .trim()
         .lines()
-        .map(|line| load_input(line.as_bytes(), &mut slab).1)
+        .map(|line| load_input(line.as_bytes()))
         .collect::<Vec<_>>();
-    let slab = slab;
 
     let p1 = {
-        let mut slab = slab.clone();
         let mut terms = terms.iter().cloned();
         let head = terms.next().unwrap();
         let foo = terms.fold(head, |a, b| {
-            let mut c = Structure::Pair(Box::new(a), Box::new(b));
-            reduce(&mut slab, &mut c);
+            let mut c = vec![Item::LeftBracket];
+            c.extend(a);
+            c.extend(b);
+            c.push(Item::RightBracket);
+            reduce(&mut c);
             c
         });
-        mag(&slab, &foo)
+        mag(&foo)
     };
 
     let mut p2 = 0;
@@ -154,10 +172,12 @@ pub fn solve() -> (impl Display, impl Display) {
             if a == b {
                 continue;
             }
-            let mut slab = slab.clone();
-            let mut c = Structure::Pair(Box::new(a.clone()), Box::new(b.clone()));
-            reduce(&mut slab, &mut c);
-            let m = mag(&slab, &c);
+            let mut c = vec![Item::LeftBracket];
+            c.extend(a);
+            c.extend(b);
+            c.push(Item::RightBracket);
+            reduce(&mut c);
+            let m = mag(&c);
             p2 = p2.max(m);
         }
     }
